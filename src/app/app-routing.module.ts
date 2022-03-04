45fa62abff42cdb1bd9632230338fb269d21354c
JRicardoP <ricardo.pachecoescobar@gmail.com>
2022-03-04 15:27:17 -0500

    Control-clientes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConfiguracionComponent } from './componentes/configuracion/configuracion.component';
+import { EditarClienteComponent } from './componentes/editar-cliente/editar-cliente.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { NoEncontradoComponent } from './componentes/no-encontrado/no-encontrado.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
+import { TableroComponent } from './componentes/tablero/tablero.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: TableroComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'registro', component: RegistroComponent},
+  {path: 'configuracion', component: ConfiguracionComponent},
+  {path: 'cliente/editar/:id', component: EditarClienteComponent},
+  {path: '**', component: NoEncontradoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
